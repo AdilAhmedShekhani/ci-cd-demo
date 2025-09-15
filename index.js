0107d147a48f7ede97b99d78c1de693ded92e5af
AdilAhmedShekhani <adilaziz2009@live.com>
2025-09-15 22:44:19 +0500

    Add CI/CD workflow

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello from Node.js project!");
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Adil! Server chal raha hai 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

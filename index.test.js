b01df4bb4739b3c1abd7c1a191d1393626369ac8
AdilAhmedShekhani <adilaziz2009@live.com>
2025-09-16 23:14:04 +0500

    Update code

--- index.test.js
+++ index.test.js
@@ -1,3 +1,12 @@
-test("simple math works", () => {
-    expect(2 + 2).toBe(4);
+// index.test.js
+
+// Simple addition function for testing
+function add(a, b) {
+  return a + b;
+}
+
+// Jest test case
+test("adds 2 + 3 to equal 5", () => {
+  const result = add(2, 3);
+  expect(result).toBe(5);
 });
